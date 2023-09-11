57ea512ac9ad860737f13ae612e567c98de8c1ac
Lukas Monico <lmonico@users.noreply.github.com>
2023-09-11 17:35:20 -0600

    Updated python script to export data to json file

--- extract_data.py
+++ extract_data.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 csv_file_path = 'spotify-2023.csv'
 
@@ -14,6 +15,13 @@ header = csv_data[0]
 
 data = csv_data[1:]
 
-print("data:")
-for row in data[:]:
-    print(row)
+count = 0
+
+for element in data:
+    print(element)
+    count = count +1
+
+json_filename = 'spotify_list.json'
+
+with open(json_filename, 'w') as json_file:
+    json.dump(data, json_file)
